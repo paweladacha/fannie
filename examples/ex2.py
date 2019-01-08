f9b009027bad803d707894373d6edd6c5b625672
Pawel <46387644+paweladacha@users.noreply.github.com>
2019-01-08 20:06:11 +0100

    Add files via upload

--- examples/ex2.py
+++ examples/ex2.py
@@ -6,20 +6,20 @@ sample = ''' - [primes](https://github.com/paweladacha/primes)
 # fx.a = fan # for array
 # fx.v = pin # for vertical
 
-get_fields = fx.p(fx.a,[
+get_fields = fx.p(fx.a, [
     lambda ar:ar[0].partition('[')[-1],
-    lambda ar:ar[1].partition(')')[0], ]) # -> generator
+    lambda ar:ar[1].partition(')')[0], ])  # -> generator
 
-get_item_dict = fx.p(fx.v,[
+get_item_dict = fx.p(fx.v, [
     get_fields,
-    lambda i:dict(zip(('title','url'),i)) ]) # -> dict
+    lambda i:dict(zip(('title', 'url'), i))])  # -> dict
 
-parse_single_item = fx.p(fx.v,[
-    fx.p(str.split,fx.x,']('),
-    get_item_dict ]) # -> dict
+parse_single_item = fx.p(fx.v, [
+    fx.p(str.split, fx.x, ']('),
+    get_item_dict])  # -> dict
 
-parse_input = fx.p(fx.v,[str.splitlines,
-    fx.p(map,parse_single_item),list]) # -> list
+parse_input = fx.p(fx.v, [str.splitlines,
+                          fx.p(map, parse_single_item), list])  # -> list
 
 parse_input(sample)
 # ->
